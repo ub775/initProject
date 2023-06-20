cc7a059010ba9ede9c6c9130eec13df410a8fb01
ub775 <kmgni@naver.com>
2023-06-20 20:22:55 +0900

    Initial commit

---
+++ src/main/java/com/springboot/init/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.springboot.init.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping
+    public String getProduct() {
+        return "Product";
+    }
+
+
+}
