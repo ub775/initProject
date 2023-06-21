40bcbd755cda7302674a787156570e376717a661
ub775 <kmgni@naver.com>
2023-06-21 19:01:38 +0900

    BaseEntity, Product Entity class 생성

---
+++ src/main/java/com/springboot/init/data/entity/Product.java
@@ -0,0 +1,29 @@
+package com.springboot.init.data.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "product")
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+}
