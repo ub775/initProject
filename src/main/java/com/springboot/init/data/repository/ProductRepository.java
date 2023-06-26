3a3488a32d52e76aff8a612eb039c8f7c0833cca
ub775 <kmgni@naver.com>
2023-06-26 20:40:14 +0900

    JPA 쿼리 메소드 추가

--- src/main/java/com/springboot/init/data/repository/ProductRepository.java
+++ src/main/java/com/springboot/init/data/repository/ProductRepository.java
@@ -1,8 +1,73 @@
 package com.springboot.init.data.repository;
 
+import com.springboot.init.controller.ProductController;
 import com.springboot.init.data.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Optional<Product> findByNumber(Long number);
+    List<Product> findAllByName(String name);
+    Product queryByNumber(Long number);
+
+    boolean existsByNumber(Long number);
+
+    long countByName(String name);
+
+    void deleteByNumber(Long number);
+    long removeByName(String name);
+
+    List<Product> findFirst5ByName(String name);
+    List<Product> findTop5ByName(String name);
+
+    Product findByNumberIs(Long number);
+    Product findByNumberEquals(Long number);
+
+    Product findByNumberIsNot(Long number);
+    Product findByNumberNot(Long number);
+
+    List<Product> findByUpdatedAtNull();
+    List<Product> findByUpdatedAtIsNull();
+    List<Product> findByUpdatedAtNotNull();
+    List<Product> findByUpdatedAtIsNotNull();
+
+    Product findByNumberAndName(Long number, String name);
+    Product findByNumberOrName(Long number, String name);
+
+    List<Product> findByPriceIsGreaterThan(Long price);
+    List<Product> findByPriceGreaterThan(Long price);
+    List<Product> findByPriceGreaterThanEqual(Long price);
+    List<Product> findByPriceIsLessThan(Long price);
+    List<Product> findByPriceLessThan(Long price);
+    List<Product> findByPriceLessThanEqual(Long price);
+    List<Product> findByPriceIsBetween(Long lowPrice, Long highPrice);
+    List<Product> findByPriceBetween(Long lowPrice, Long highPrice);
+
+    List<Product> findByNameLike(String name);
+    List<Product> findByNameIsLike(String name);
+
+    List<Product> findByNameContains(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByNameIsContaining(String name);
+
+    List<Product> findByNameStartsWith(String name);
+    List<Product> findByNameStartingWith(String name);
+    List<Product> findByNameIsStartingWith(String name);
+
+    List<Product> findByNameEndsWith(String name);
+    List<Product> findByNameEndingWith(String name);
+    List<Product> findByNameIsEndingWith(String name);
+
+    List<Product> findByNameOrderByNumberAsc(String name);
+    List<Product> findByNameOrderByNumberDesc(String name);
+
+    List<Product> findByName(String name, Sort sort);
+
+    Page<Product> findByName(String name, Pageable pageable);
 }
--- src/test/java/com/springboot/init/data/repository/ProductRepositoryTest.java
+++ src/test/java/com/springboot/init/data/repository/ProductRepositoryTest.java
@@ -4,6 +4,12 @@ import com.springboot.init.data.entity.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -47,5 +53,38 @@ public class ProductRepositoryTest {
         assertEquals(product.getName(), foundProduct.getName());
         assertEquals(product.getPrice(), foundProduct.getPrice());
         assertEquals(product.getStock(), foundProduct.getStock());
+    }
+
+    @Test
+    void sortingAndPagingTest() {
+        //given
+        Product product1 = new Product();
+        product1.setName("펜");
+        product1.setPrice(1000);
+        product1.setStock(100);
+
+        Product product2 = new Product();
+        product2.setName("펜");
+        product2.setPrice(5000);
+        product2.setStock(300);
+
+        Product product3 = new Product();
+        product3.setName("펜");
+        product3.setPrice(500);
+        product3.setStock(50);
+
+        Product savedProduct1 = productRepository.save(product1);
+        Product savedProduct2 = productRepository.save(product2);
+        Product savedProduct3 = productRepository.save(product3);
+
+        System.out.println(productRepository.findByName("펜", getSort()));
+        System.out.println(productRepository.findByName("펜", PageRequest.of(0, 2)));
+    }
+
+    private Sort getSort() {
+        return Sort.by(
+                Order.asc("price"),
+                Order.desc("stock")
+        );
     }
 }
