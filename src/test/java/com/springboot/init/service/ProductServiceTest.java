d61d2d73936b57e60720b8134d12b43eb18ec5a1
ub775 <kmgni@naver.com>
2023-06-26 18:34:47 +0900

    Controller, Service, Repository Test

---
+++ src/test/java/com/springboot/init/service/ProductServiceTest.java
@@ -0,0 +1,65 @@
+package com.springboot.init.service;
+
+import com.springboot.init.data.dto.ProductDto;
+import com.springboot.init.data.dto.ProductResponseDto;
+import com.springboot.init.data.entity.Product;
+import com.springboot.init.data.repository.ProductRepository;
+import com.springboot.init.service.impl.ProductServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+
+public class ProductServiceTest {
+
+    private final ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+    private ProductServiceImpl productService;
+
+    @BeforeEach
+    public void setUpTest() {
+        productService = new ProductServiceImpl(productRepository);
+    }
+
+    @Test
+    void getProductTest() {
+        Product givenProduct = new Product();
+        givenProduct.setNumber(123L);
+        givenProduct.setName("펜");
+        givenProduct.setPrice(1000);
+        givenProduct.setStock(1234);
+
+        Mockito.when(productRepository.findById(123L))
+                .thenReturn(Optional.of(givenProduct));
+
+        ProductResponseDto productResponseDto = productService.getProduct(123L);
+
+        Assertions.assertEquals(productResponseDto.getNumber(), givenProduct.getNumber());
+        Assertions.assertEquals(productResponseDto.getName(), givenProduct.getName());
+        Assertions.assertEquals(productResponseDto.getPrice(), givenProduct.getPrice());
+        Assertions.assertEquals(productResponseDto.getStock(), givenProduct.getStock());
+
+        verify(productRepository).findById(123L);
+    }
+
+    @Test
+    void saveProductTest() {
+        Mockito.when(productRepository.save(any(Product.class)))
+                .then(returnsFirstArg());
+
+        ProductResponseDto productResponseDto = productService.saveProduct(
+                new ProductDto("펜", 1000, 1234));
+
+        Assertions.assertEquals(productResponseDto.getName(), "펜");
+        Assertions.assertEquals(productResponseDto.getPrice(), 1000);
+        Assertions.assertEquals(productResponseDto.getStock(), 1234);
+
+        verify(productRepository).save(any());
+    }
+}
